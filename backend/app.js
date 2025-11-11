43bb6f4896955d0080eae54734533212fb6fde07
ahmed bahaa <ahmedbahaa948@gmail.com>
2025-11-11 16:55:20 +0200

    edited mafrod esht8l

--- backend/app.js
+++ backend/app.js
@@ -15,17 +15,19 @@ const allowedOrigins =
 
 
 // Configure CORS with environment variable
-app.use(cors({
-  origin: function(origin, callback) {
-    // allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      return callback(new Error('The CORS policy for this site does not allow access from the specified Origin.'), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true
-}))
+app.use(cors(
+//   {
+//   origin: function(origin, callback) {
+//     // allow requests with no origin (like mobile apps or curl requests)
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       return callback(new Error('The CORS policy for this site does not allow access from the specified Origin.'), false);
+//     }
+//     return callback(null, true);
+//   },
+//   credentials: true
+// }
+))
 app.use(express.json())
 
 // Connect to MongoDB
